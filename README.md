abe59a6c718d6891d79eaa4e6b14d4db47e090c9
Brian Antunes <brian.antunes@yahoo.com>
2022-05-13 14:09:14 -0400

    Updated main.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TwitchPiTv
  
+Creating an application to watch Twitch Streams in a TV style way on a Raspberry Pi.
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import requests
 import yaml
+import json
 from streamlink import Streamlink
 
+# Load API keys
 config = yaml.safe_load(open("config.yml"))
 
 URL = "https://id.twitch.tv/oauth2/token"
@@ -10,6 +12,10 @@ CLIENT_ID = config['client_id']
 CLIENT_SECRET = config['client_secret']
 GRANT_TYPE = "client_credentials"
 
+# Setup Streamlink
+session = Streamlink()
+
+# Set Params to get access token using Client secret
 PARAMS = {
   "client_id": CLIENT_ID,
   "client_secret": CLIENT_SECRET,
@@ -20,12 +26,27 @@ r1 = requests.post(url = URL, params = PARAMS)
 
 token = r1.json()["access_token"]
 
+# Set Header with Client ID and Access Token
 headers = {
     'Client-ID': CLIENT_ID,
     'Authorization': 'Bearer ' + token
 }
 
+# Make GET request from Twitch
 streamer_name = "iitztimmy"
-
 stream = requests.get('https://api.twitch.tv/helix/streams?user_login=' + streamer_name, headers=headers)
-print(stream)
+
+# Get channels being followed
+followed = requests.get("https://api.twitch.tv/helix/streams/followed", headers=headers)
+obj = json.loads(followed)
+
+name = []
+for user in obj['data']:
+    if 'user_id' in user:
+        name.append(user['user_id'])
+
+print(' '.join(name))
+
+
+# Extract streams
+streams = session.streams("https://twitch.tv/{}".format(streamer_name))

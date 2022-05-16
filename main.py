f2a258ff97b0a994be0616e18f55ba06bdc1681f
Brian Antunes <brian.antunes@yahoo.com>
2022-05-15 20:52:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import yaml
 import json
 import time
+import keyboard
 import subprocess
 from streamlink import Streamlink
 
@@ -24,7 +25,6 @@ PARAMS = {
   "grant_type": GRANT_TYPE
 }
 
-
 r1 = requests.post(url = URL, params = PARAMS)
 
 token = r1.json()["access_token"]
@@ -36,37 +36,67 @@ headers = {
 }
 
 # Used to get user's ID
-# user = requests.get('https://api.twitch.tv/helix/users?login=brian92617', headers=headers)
+# user = requests.get('https://api.twitch.tv/helix/users?login=YourUserName', headers=headers)
 # print(user.text)
 
-# Get channels being followed and output all current live channels
-followed = requests.get('https://api.twitch.tv/helix/users/follows?from_id=206770467&first=100', headers=headers)
-obj = json.loads(followed.text)
+def get_live_channels(headers):
+    # Get channels being followed and output all current live channels in order by view count
+    followed = requests.get('https://api.twitch.tv/helix/users/follows?from_id=206770467&first=100', headers=headers)
+    obj = json.loads(followed.text)
 
-name=[]
-liveCheck = "https://api.twitch.tv/helix/streams?"
+    liveCheck = "https://api.twitch.tv/helix/streams?"
 
-# Go through all followed channels
-for user in obj['data']:
-    if 'to_name' in user:
-        liveCheck += "user_login=" + user['to_name'] + "&"
+    # Go through all followed channels
+    for user in obj['data']:
+        if 'to_name' in user:
+            liveCheck += "user_login=" + user['to_name'] + "&"
 
- # Check if followed channels are live
-check = requests.get(liveCheck, headers=headers)
-checkTest = json.loads(check.text)
-for live in checkTest['data']:
-    if live['type'] == "live":
-        print(live['user_login'] + " is " + live['type'] + " with " + str(live['viewer_count']) + " viewers")
-        name.append(live['user_login'])
-        
+     # Check if followed channels are live
+    name=[]
+    check = requests.get(liveCheck, headers=headers)
+    checkTest = json.loads(check.text)
+    for live in checkTest['data']:
+        if live['type'] == "live":
+            # print(live['user_login'] + " is " + live['type'] + " with " + str(live['viewer_count']) + " viewers")
+            name.append(live['user_login'])
+    return name
 
-# Play stream
-for names in name:
-    process = subprocess.Popen('streamlink --player-args "--fullscreen --play-and-exit" https://twitch.tv/{} best'.format(names))
-    try:
-        print('Running in process', process.pid)
-        process.wait(timeout=10)
-    except subprocess.TimeoutExpired:
-        print('Timed out - killing', process.pid)
+def play_stream(streamer):
+    stream_to_play = 'streamlink --player-args "--fullscreen --play-and-exit" https://twitch.tv/{} best'.format(streamer)
+    return stream_to_play
+
+# Set streamer to first streamer, start by playing the first stream
+currentStreamer = 0
+name = get_live_channels(headers)
+process = subprocess.Popen('streamlink --player-args "--fullscreen --play-and-exit" https://twitch.tv/{} best'.format(name[currentStreamer]))
+
+while True:
+    # Wait for an event, if event was a key down and was N go to next streamer
+    event = keyboard.read_event()
+    if event.event_type == keyboard.KEY_DOWN and event.name == 'n':
+        # Refresh live channels
+        name = get_live_channels(headers)
+        if currentStreamer == (len(name) - 1):
+            currentStreamer = 0
+            process.terminate()
+            process = subprocess.Popen(play_stream(name[currentStreamer]))
+        else:
+            currentStreamer += 1
+            process.terminate()
+            process = subprocess.Popen(play_stream(name[currentStreamer]))
+    # if B was pressed, go to last streamer
+    elif event.event_type == keyboard.KEY_DOWN and event.name == 'b':
+        # Refresh live channels
+        name = get_live_channels(headers)
+        if currentStreamer == 0:
+            currentStreamer = (len(name) - 1)
+            process.terminate()
+            process = subprocess.Popen(play_stream(name[currentStreamer]))
+        else:
+            currentStreamer -= 1
+            process.terminate()
+            process = subprocess.Popen(play_stream(name[currentStreamer]))
+    # Exit if C was pressed
+    elif event.event_type == keyboard.KEY_DOWN and event.name == 'c':
         process.terminate()
-    print("Done")
+        break
